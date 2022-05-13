98a61ddc8e6ffb3c6836441a6a32184cfce90720
Melissa Paul <paulmeli@umich.edu>
2022-05-13 15:24:02 -0400

    Only allow user select text file

--- counter.py
+++ counter.py
@@ -3,7 +3,7 @@ from tkinter import Tk     # from tkinter import Tk for Python 3.x
 from tkinter.filedialog import askopenfilename
 
 Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
-filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
+filename = askopenfilename(filetypes=[("Text files", "*.txt"), ("All Files", "*.*")]) # show an "Open" dialog box and return the path to the selected file
 print(filename)
 
 # Read syllabus file
